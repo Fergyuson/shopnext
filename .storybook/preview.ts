116cec2c6b1a19a7aeb9064e712ec571846c97ae
Fergyuson <magicklessons@gmail.com>
2025-06-14 19:04:45 +0300

    Инициализовали базовый стек, структуриовали по FSD, подключили Storybook, интегрировали apolloClient и граф -ql

---
+++ .storybook/preview.ts
@@ -0,0 +1,13 @@
+// .storybook/preview.ts
+import "../src/app/globals.css";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/i,
+    },
+  },
+  a11y: { test: "todo" },
+};
