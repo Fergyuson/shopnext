cb94d590ba7f5fd437e91adc908da741a70aeb9c
Fergyuson <magicklessons@gmail.com>
2025-06-14 19:04:45 +0300

    Инициализовали базовый стек, структуриовали по FSD, подключили Storybook, интегрировали apolloClient и граф -ql

---
+++ .storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from '@storybook/experimental-nextjs-vite';
+
+const config: StorybookConfig = {
+  stories: [
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/*.mdx'
+  ],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y'
+  ],
+  framework: {
+    name: '@storybook/experimental-nextjs-vite',
+    options: {},
+  },
+  staticDirs: ['../public'],
+};
+
+export default config;
